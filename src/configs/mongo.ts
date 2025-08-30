f2ed994a01205b9a03ed7cecf180301eba2f512b
mark angelo <76676425+ignacio2366@users.noreply.github.com>
2025-08-30 19:20:43 +0800

    feat: mongoose

--- src/configs/mongo.ts
+++ src/configs/mongo.ts
@@ -1,4 +1,5 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
+import mongoose, { Schema, Document } from "mongoose";
 
 export const client = new MongoClient(
   `mongodb+srv://markangeloignacio001:pEN5qF4cGae9jVPS@sampledb.fvsobra.mongodb.net/?retryWrites=true&w=majority&appName=SampleDB`,
@@ -9,6 +10,32 @@ export const client = new MongoClient(
       deprecationErrors: true,
     },
   }
+);
+
+const schemaUsers = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+  hobbies: [String],
+});
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  hobbies: string[];
+}
+
+// Define schema
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  hobbies: { type: [String], required: true },
+});
+
+export const User = mongoose.model<IUser>(
+  "sampleUser",
+  userSchema,
+  "User Details"
 );
 
 async function connectDB() {
